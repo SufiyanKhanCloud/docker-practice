ee7a3a3bb25da96c802e0c6fedda8babd82954c3
Ubuntu <ubuntu@ip-172-31-1-250.us-east-2.compute.internal>
2025-04-29 18:46:06 +0000

    Added simple-java-docker project

---
+++ simple-java-docker/src/Main.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        Date currentDate = new Date();
+        System.out.println("Hello, Sufiyan here learning docker! Current date: " + currentDate);
+    }
+}
